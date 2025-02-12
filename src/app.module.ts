a9786115e7365173253c4c2c5912e4af4911f3ee
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-02-12 13:12:38 +0700

    Initialize project

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGO_URI');
+        if (!uri) {
+          throw new Error('MongoDB URI không được cung cấp!');
+        }
+        return { uri };
+      },
+    }),
+    CloudinaryModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
